7d84afae9804ea5632753502e51759163fe922d3
Mark Price <mark.price@ons.gov.uk>
2022-10-05 11:26:21 +0100

    Introduce Python 3.9 build

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     classifiers=[
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
     ],
     packages=[
