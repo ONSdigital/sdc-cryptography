819b4d92ddc12fee5a5e2e3a5dcf11382242c694
Mark Price <mark.price@ons.gov.uk>
2022-10-05 08:46:41 +0100

    Removing other Python versions as causing job to be cancelled

--- setup.py
+++ setup.py
@@ -39,10 +39,6 @@ setup(
     classifiers=[
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
     ],
     packages=[
